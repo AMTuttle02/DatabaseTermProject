1f90aff538291473ef32e2871eb7bc8d18c14930
mruple <92047759+mruple@users.noreply.github.com>
2021-10-22 15:38:59 -0400

    Create databaseSetup.sql

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -24,20 +24,15 @@ CREATE TABLE classroomInfo (
   courseNum INT(11)
 );
 
-CREATE TABLE userInput (
-  userID PRIMARY KEY,
-  startTime VARCHAR(5),
-  endTime VARCHAR(5),
-  roomID VARCHAR(6),
-  FOREIGN KEY(roomID) REFERENCES classroomInfo(roomID)
-  ON DELETE SET NULL
-  ON UPDATE CASCADE
-);
-
 insert into courseInfo values
-('7700201', 'American Sign Language III', '15:30', '16:45', 'POL434'),
+('7700201', 'American Sign Language III', '15:30', '16:45', 'Pol434'),
 ('3460316', 'Data Structures', '12:15', '13:30', 'CAS134'),
 ('3470461', 'Applied Statistics', '14:00', '15:15', 'CAS140'),
 ('3460475', 'Database Management', '15:30', '16:45', 'CAS134');
+
+insert into classroomInfo values
+('Pol', '434', 'Polsky', '30'),
+('CAS', '134', 'College of Arts & Sciences', '30'),
+('CAS', '134', 'College of Arts & Sciences', '30');
 
 

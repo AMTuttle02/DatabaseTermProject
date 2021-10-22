0c48b33bb8b08b57d5c51f5af9320c0ba935e872
Alex Tuttle <89881964+AMTuttle02@users.noreply.github.com>
2021-10-22 15:32:10 -0400

    Update databaseSetup.sql

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -7,15 +7,25 @@ DROP TABLE IF EXISTS courseInfo;
 CREATE TABLE courseInfo (
   courseNum INT(11),
   courseName VARCHAR(100),
-  startTime TIME,
-  endTime Time,
-  PRIMARY KEY (courseNum)
+  startTime VARCHAR(5),
+  endTime VARCHAR(5),
+  roomID VARCHAR(6),
+  PRIMARY KEY (courseNum),
+  FOREIGN KEY(roomID) REFERENCES classroomInfo(roomID)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE
+);
+
+CREATE TABLE classroomInfo (
+  roomID INT(6) PRIMARY KEY,
+  roomNum INT(3),
+  location VARCHAR(100),
+  capacity INT(3),
+  courseNum INT(11)
 );
 
 insert into courseInfo values
-(
-  ('7700201', 'American Sign Language III', '3:30', '4:45'),
-  ('3460316', 'Data Structures', '12:15', '1:30'),
-  ('3470461', 'Applied Statistics', '2:00', '3:15'),
-  ('3460475', 'Database Management', '3:30', '4:45')
-);
+('7700201', 'American Sign Language III', '15:30', '16:45'),
+('3460316', 'Data Structures', '12:15', '13:30'),
+('3470461', 'Applied Statistics', '14:00', '15:15'),
+('3460475', 'Database Management', '15:30', '16:45');

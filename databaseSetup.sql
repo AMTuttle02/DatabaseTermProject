e8de70327d682680586d73d305fcdc218015b48d
Alex Tuttle <89881964+AMTuttle02@users.noreply.github.com>
2021-10-22 15:08:18 -0400

    Create databaseSetup.sql

---
+++ databaseSetup.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE;
+
+USE dbmang;
+
+DROP TABLE IF EXISTS courseInfo;
+
+CREATE TABLE courseInfo (
+  courseNum INT(11),
+  courseName VARCHAR(100),
+  startTime TIME,
+  endTime Time,
+  PRIMARY KEY (courseNum)
+);
+
+insert into courseInfo values
+(
+
+
+);

fb97c3d33bc50a76ae159998940e5224350716b5
Alex Tuttle <89881964+AMTuttle02@users.noreply.github.com>
2021-12-03 13:55:25 -0500

    Delete dbmang directory

--- dbmang/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-// Redirect to the login page:
-echo '<h1>Log Out Succesful</h1>
-	<h1> Click <a href="index.html">Here</a> To Return to Login Screen</h1>'
-?>
